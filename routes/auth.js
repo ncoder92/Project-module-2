caab89d30d83755a05b83311f1e9d2c3f295491a
Victor Pellerin <pellerin.victor@gmail.com>
2018-02-09 15:20:55 +0100

    login and logout working

--- routes/auth.js
+++ routes/auth.js
@@ -8,56 +8,57 @@ const router = express.Router();
 const bcryptSalt = 10;
 
 /* GET login page. */
-// router.get('/login', function (req, res, next) {
-//   if (req.session.currentUser) {
-//     return res.redirect('/');
-//   }
-//   res.render('auth/login', {
-//     title: 'Login'
-//   });
-// });
+router.get('/login', function (req, res, next) {
+  if (req.session.currentUser) {
+    return res.redirect('/');
+  }
+  const data = {
+    usernameField: ''
+  };
+  res.render('auth/login', data);
+});
 
-// /* POST login page. */
-// router.post('/login', (req, res, next) => {
-//   if (req.session.currentUser) {
-//     return res.redirect('/');
-//   }
+/* POST login page. */
+router.post('/login', (req, res, next) => {
+  if (req.session.currentUser) {
+    return res.redirect('/');
+  }
 
-//   var username = req.body.username;
-//   var password = req.body.password;
+  var username = req.body.username;
+  var password = req.body.password;
 
-//   if (username === '' || password === '') {
-//     const data = {
-//       title: 'Login',
-//       message: 'Indicate a username and a password to sign up'
-//     };
-//     return res.render('auth/login', data);
-//   }
+  if (username === '' || password === '') {
+    const data = {
+      usernameField: username,
+      message: 'Indicate a username and a password to login'
+    };
+    return res.render('auth/login', data);
+  }
 
-//   User.findOne({ 'username': username }, (err, user) => {
-//     if (err) {
-//       return next(err);
-//     }
-//     if (!user) {
-//       const data = {
-//         title: 'Login',
-//         message: 'Username or password are incorrect'
-//       };
-//       return res.render('auth/login', data);
-//     }
+  User.findOne({ 'username': username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      const data = {
+        usernameField: username,
+        message: 'Username or password are incorrect'
+      };
+      return res.render('auth/login', data);
+    }
 
-//     if (bcrypt.compareSync(password, user.password)) {
-//       req.session.currentUser = user;
-//       res.redirect('/');
-//     } else {
-//       const data = {
-//         title: 'Login',
-//         message: 'Username or password are incorrect'
-//       };
-//       res.render('auth/login', data);
-//     }
-//   });
-// });
+    if (bcrypt.compareSync(password, user.password)) {
+      req.session.currentUser = user;
+      res.redirect('/');
+    } else {
+      const data = {
+        usernameField: username,
+        message: 'Username or password are incorrect'
+      };
+      res.render('auth/login', data);
+    }
+  });
+});
 
 /* render the signup form. */
 router.get('/signup', (req, res, next) => {
