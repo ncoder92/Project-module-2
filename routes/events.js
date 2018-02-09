e33143d96c18500f761935ec5bea521189d7e38b
Victor Pellerin <pellerin.victor@gmail.com>
2018-02-09 17:06:15 +0100

    GET new event and POST event done

--- routes/events.js
+++ routes/events.js
@@ -9,8 +9,40 @@ router.get('/', function (req, res, next) {
 });
 
 // GET ('/events/new')
+router.get('/new', (req, res, next) => {
+  if (!req.session.currentUser) {
+    return res.redirect('/');
+  }
+  res.render('events/new');
+});
 
 // POST ('/events') Crear nuevo
+router.post('/', (req, res, next) => {
+  const title = req.body.title;
+  const description = req.body.description;
+
+  if (title === '') {
+    const data = {
+      message: 'Title cannot be empty'
+    };
+    return res.render('events/new', data);
+  }
+  if (description === '') {
+    const data = {
+      message: 'Description cannot be empty'
+    };
+    return res.render('events/new', data);
+  }
+
+  const newEvent = new Event({
+    title,
+    description
+  });
+
+  newEvent.save()
+    .then(() => res.redirect('/'))
+    .catch(next);
+});
 
 // GET ('/events/:Id')
 
