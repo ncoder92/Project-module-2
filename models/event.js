8f12540e5f75cc0e1d7de364b5680cfed0d0d462
Victor Pellerin <pellerin.victor@gmail.com>
2018-02-12 15:52:48 +0100

    delete button working

--- models/event.js
+++ models/event.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const objectId = Schema.Types.ObjectId;
 
 const EventSchema = new Schema({
   title: String,
   description: String,
-  attendees: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+  attendees: [{ type: objectId, ref: 'User' }],
+  active: {
+    type: Boolean,
+    default: true
+  }
 });
 
 const Event = mongoose.model('Event', EventSchema);
--- routes/profile.js
+++ routes/profile.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const User = require('../models/user');
+const Event = require('../models/event');
 
 // GET ('/profile/myevents')
 router.get('/my-events/:userId', (req, res, next) => {
@@ -17,5 +18,17 @@ router.get('/my-events/:userId', (req, res, next) => {
 });
 
 // POST ('/profile/myevents/:Id')
+router.post('/my-events/:eventId', (req, res, next) => {
+  const eventId = req.params.eventId;
+  Event.findByIdAndUpdate(eventId, { $set: { active: false } })
+    .then((eventData) => {
+      const data = {
+        events: eventData
+      };
+      console.log(req.session.currentUser);
+      res.redirect(`/profile/my-events/${req.session.currentUser._id}`);
+    })
+    .catch(next);
+});
 
 module.exports = router;
